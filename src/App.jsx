743c3132bbb5b4a210016f9a46ebe5acac87c750
RAYSSA GABRIELLA RODRIGUES DA SILVA <25162449@edu119.local>
2026-02-27 10:53:43 -0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {Heading} from './component/Heading.jsx'
+  export function App(){
+    return (
+      <>
+        <h1>Rayssa🥶</h1>
+        <div>
+          <Heading />
+        </div>
+      </>
+    )
+  }
